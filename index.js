064c6c7ddc1cb3cfc4c77c9c67d210d94f9b8c18
Anna Boykova <boykova_anuta@mail.ru>
2018-11-07 05:21:49 +0300

    Added a possibility of signal receiving from
    remote controller after pressing on some button

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const server = require("./src/backend/server/server")
 const db = require('./src/backend/db/db')
+const lirc = require('./src/backend/lirc/lirc')
 
 db.init()
+lirc.init(db)
 const host = "0.0.0.0"
 const port = 11296
 server.launch(db, port, host)
---
+++ src/backend/data/button.action.js
@@ -0,0 +1,31 @@
+const buttons_dictionary = require('./button.all')
+
+class ButtonAction {
+    constructor(type, remote) {
+        this.type = type
+        this.remote = remote
+        this.time = new Date()
+    }
+
+    toString() {
+        return `ButtonAction: type=${this.type}, remote=${this.remote}, time=${this.time}`
+    }
+
+    checkSupporting() {
+        if (buttons_dictionary.hasOwnProperty(remote)) {
+            for (var i = 0; i < buttons_dictionary[remote].length; i++) {
+                if (this.type === buttons_dictionary[this.remote][i]) {
+                    return true
+                }
+            }
+        } else {
+            console.log("This remote device \'" + this.remote + "\' isn't supported")
+            return false
+        }
+
+        console.log("This button type \'" + this.type + "\' isn't supported")
+        return false
+    }
+}
+
+module.exports = ButtonAction
